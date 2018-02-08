a232ad9c2ff4edef1fcf7fa4a1ac0b5932fdcf8a
Matej <vrabel.mat@gmail.com>
2018-02-08 22:31:40 +0100

    $ The code has been edited to match Programming Assignment 2 goals.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Double function using Lexical scoping concept to calculate a matrix inversion
 
-## Write a short comment describing this function
+## makeCacheMatrix function - to store objects/variables, 
+##inputs as square matrices only
+
+## cacheSolve function - to calculate the inversion of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        sol <- NULL
+        set <- function(y) {
+                x <<- y
+                sol <<- NULL
+        }
+        get <- function() x
+        setSolv <- function(solv) sol <<- solv
+        getSolv <- function() sol
+        list(set = set, get = get,
+             setSolv = setSolv,
+             getSolv = getSolv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        sol <- x$getSolv()
+        if(!is.null(sol)) {
+                message("getting cached data for matrix inversion")
+                return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setSolv(sol)
+        sol
 }
